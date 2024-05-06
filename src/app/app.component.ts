e86aa0a9f549d88bc3c092a4be041910f9f2864f
Achanandhi-M <achanandhi.m@gmail.com>
2024-05-06 17:59:33 +0530

    first-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { RouterOutlet,RouterLink,RouterLinkActive } from '@angular/router';
+import { HeroesListComponent } from './heroes-list/heroes-list.component';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, HeroesListComponent,CrisisListComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Angular-router';
